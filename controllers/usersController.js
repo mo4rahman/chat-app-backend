182bcc9a2eeac2516a5b268b5904cd4e8b5af742
mo4rahman <mo4rahman@gmail.com>
2022-06-13 02:25:03 -0400

    finish avatar component

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -53,3 +53,39 @@ module.exports.login = async (req, res, next) => {
     next(err);
   }
 };
+
+// module.exports.setAvatar = async (req, res, next) => {
+//   try {
+//     const userId = req.params.id;
+//     const avatarImage = req.body.image;
+//     const userData = await User.findByIdAndUpdate(userId, {
+//       isAvatarImageSet: true,
+//       avatarImage,
+//     });
+//     return res.json({
+//       isSet: userData.isAvatarImageSet,
+//       image: userData.avatarImage,
+//     });
+//   } catch (err) {
+//     next(err);
+//   }
+// };
+
+module.exports.setAvatar = async (req, res, next) => {
+  try {
+    const userId = req.params.id;
+    const avatarImage = req.body.image;
+    const userData = await User.findOneAndUpdate(userId, {
+      isAvatarImageSet: true,
+      avatarImage,
+    });
+    if (userData) {
+      return res.json({
+        isSet: userData.isAvatarImageSet,
+        image: userData.avatarImage,
+      });
+    }
+  } catch (err) {
+    next(err);
+  }
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,9 @@
-const { signup, login } = require("../controllers/usersController");
+const { signup, login, setAvatar } = require("../controllers/usersController");
 
 const router = require("express").Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
+router.post("/setAvatar/:id", setAvatar);
 
 module.exports = router;
